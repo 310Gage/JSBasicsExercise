56969304386edfa8edf85c90583d4922d9ef0d77
Gage Hall <Gageh310@gmail.com>
2022-09-30 09:56:10 -0400

    completed JSBasicsExercise

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log(310);
+
+// Number
+// String
+// Boolean
+// Null
+// Undefined
+
+// +
+// -
+// *
+// /
+// %
+// **
+
+console.log(5 % 2); 
+console.log(5 % 4); 
+
+console.log(`I'M MARY POPPINS, Y'ALL`);
+
+console.log("Hawk" + "eye");
+
+console.log("Gage".length + "Hall".length);
+
+console.log("Avengers: Endgame"[16]);
